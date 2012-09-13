c6c012282036bd6c298e0ad33234d6bf05e9c029
Andrey Ushakov <a_ushakov@ideco.ru>
2012-09-13 18:28:49 +0600

    Func tests on Storage

--- stat_sender/functional_tests_suite.py
+++ stat_sender/functional_tests_suite.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 import unittest
+from tests.functional_tests.storage.test_sqlite_storage_impl import TestSqliteStorageImpl
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestSqliteStorageImpl))
+    return suite
 
 if __name__ == '__main__':
     unittest.main()
--- stat_sender/tests/__init__.py
+++ stat_sender/tests/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 print 'BETTER DIE FOR THE EMPEROR, THAN LIVE FOR YOURSELF (c) Warhammer 40k'
 
 __author__ = 'andrey.ushakov'
