a4cc910aeb426d13a1a3d386fec4cc7ce0ce349d
Andrey Ushakov <a_ushakov@ideco.ru>
2013-02-06 16:01:16 +0600

    simple corrections

--- stat_db_funtest_utils/sqlite_db_manager.py
+++ stat_db_funtest_utils/sqlite_db_manager.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 import os
-import shutil
 import sqlite3
 import subprocess
 import db_manager
@@ -24,19 +23,16 @@ class SqliteDbManager(db_manager.DbManager):
 
     # spec: None -> None
     def _prepare_db(self):
-        os.chdir(self._initial_working_dir)
         self._create_dest_db_dir()
+        self._remove_dest_db()
         abs_create_script = os.path.abspath(self._db_create_script)
-        self._remove_dest_db_dir()
         create_result = subprocess.call(['python', abs_create_script, os.path.join(self._db_dirname, self._db_filename)])
         if create_result:
             raise db_manager.DbCreationException()
-        os.chdir(self._initial_working_dir)
 
     # spec: None -> None
     def _clear_db(self):
-        self._remove_dest_db_dir()
-        os.chdir(self._initial_working_dir)
+        self._remove_dest_db()
 
     # spec: None -> ?Connection?
     def _create_connection(self):
@@ -48,8 +44,9 @@ class SqliteDbManager(db_manager.DbManager):
             os.makedirs(self._db_dirname)
 
     # spec: None -> None
-    def _remove_dest_db_dir(self):
-        if os.path.exists(self._db_dirname):
-            shutil.rmtree(self._db_dirname)
+    def _remove_dest_db(self):
+        dest_db_filename = os.path.join(self._db_dirname, self._db_filename)
+        if os.path.exists(dest_db_filename):
+            os.unlink(dest_db_filename)
 
 __author__ = 'andrey.ushakov'
